a711ff1337d0e71acdd55c84736462b7c26f4707
Uchendumbila <ekeuche1408@gmail.com>
2024-01-09 00:59:14 -0800

    Task4 Update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python3
 def inherits_from(obj, a_class):
+
     """
     Check if the object is an instance of a class that inherited (directly or indirectly)
     from the specified class.
