47c90b095739aa734c184982aec10d65895560d1
Uchendumbila <ekeuche1408@gmail.com>
2024-01-09 06:28:21 -0800

    Advanced1and2

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
-def print_info():
-    print('File size: {:d}'.format(file_size))
-
-    for scode, code_times in sorted(status_codes.items()):
-        if code_times > 0:
-            print('{}: {:d}'.format(scode, code_times))
-
-
 status_codes = {
     '200': 0,
     '301': 0,
@@ -23,6 +14,14 @@ status_codes = {
 
 lc = 0
 file_size = 0
+
+def print_info():
+    global file_size  # Declare file_size as a global variable
+    print('File size: {:d}'.format(file_size))
+
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
 
 try:
     for line in sys.stdin:
