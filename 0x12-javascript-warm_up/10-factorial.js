d6286b41dc7ee2064c9e197bd24b0e84094ff157
Uchendumbila <ekeuche1408@gmail.com>
2024-03-17 06:49:00 -0700

    Task-10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function factorial (x) {
+  if (x <= 0) {
+    return 0;
+  } else if (x === 1) {
+    return 1;
+  } else {
+    return (x * factorial(a - 1));
+  }
+}
+
+const myInt = parseInt(process.argv[2]);
+if (isNaN(myInt)) {
+  console.log('1');
+} else {
+  console.log(factorial(myInt));
+}
